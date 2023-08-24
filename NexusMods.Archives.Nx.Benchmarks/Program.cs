c2291972a7d968478d36499afce79b3f29282e3e
sewer56lol <sewer56lol@googlemail.com>
2023-08-24 10:22:27 +0100

    [delet this]

--- NexusMods.Archives.Nx.Benchmarks/Program.cs
+++ NexusMods.Archives.Nx.Benchmarks/Program.cs
@@ -1,7 +1,6 @@
 // ReSharper disable RedundantUsingDirective
 // See https://aka.ms/new-console-template for more information
 
-using System.Diagnostics;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Jobs;
